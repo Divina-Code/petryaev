d79281b67316026d2545dce9546803bdeee00ade
alexander-petr <petryaev.al2005@mail.ru>
2020-12-27 17:28:44 +0500

    Create 02 how old are u.py

---
+++ 02 how old are u.py
@@ -0,0 +1,10 @@
+print('hello, dude!')
+
+name = input('what is ur name?    ')
+print('nice to meet u,', name)
+
+year=int(input('what year were u born in?    '))
+print('oh,',year,'was great!')
+
+age=int(2020-year)
+print('u are already',age,'!')
