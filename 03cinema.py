0202873b5066d27d33540b7cec3f954f433352ce
alexander-petr <petryaev.al2005@mail.ru>
2020-12-27 20:19:26 +0500

    Update 03cinema.py

--- 03cinema.py
+++ 03cinema.py
@@ -4,5 +4,9 @@ year = int(input('what year were u born in?  '))
 age = 2020-year
 if age >= 18:
     print('oh,', str(name)+', i have smth for u! horror might be good for such a big guy')
-else:
+elif age >= 16:
+    print('action film is a good option for u')
+elif age >= 13:
     print('comedy might be interesting for u,', name)
+else:
+    print('movie seems like a good option for u')
