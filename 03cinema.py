d722639854a2bd2f188d3082369d7533f916c05c
alexander-petr <petryaev.al2005@mail.ru>
2020-12-27 19:54:22 +0500

    Create 03cinema.py

---
+++ 03cinema.py
@@ -0,0 +1,8 @@
+print('hi, dude!')
+name = input('what is ur name?   ')
+year = int(input('what year were u born in?  '))
+age = 2020-year
+if age >= 18:
+    print('oh,', name, 'i have smth for u! horror might be good for such a big guy')
+else:
+    print('comedy might be interesting for u,', name)
