fe23397557820fb16801407e777039ceb8a785b8
alexander-petr <petryaev.al2005@mail.ru>
2020-12-28 16:08:40 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,12 +3,12 @@ guessed = False
 while guessed != True:
     answer =input('the mystery number is ')
     if answer == '40':
-        print('u are so lucky')
+        print('\nu are so lucky')
         guessed = True
     elif answer > '40':
-        print('the number is smaller')
+        print('\nthe mystery number is smaller')
     else:
-        print('the number is bigger')
-print('u DID it')
+        print('\nthe mystery number is bigger')
+print('u DID it!!!!!')
 
 
