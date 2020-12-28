1917574938c6a31c77e1b9f5df46cf895b7b41c3
alexander-petr <petryaev.al2005@mail.ru>
2020-12-28 17:33:56 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,6 +3,7 @@ from time import sleep
 print('find the mystery number')
 mysterynumber = ri(0, 100)
 guessed = False
+sleep(3)
 while guessed != True:
     answer = int(input('the mystery number is '))
     if answer == mysterynumber:
@@ -13,4 +14,4 @@ while guessed != True:
     else:
         print('\nthe mystery number is bigger')
 print('u DID it!!!!!')
-sleep(3)
+
