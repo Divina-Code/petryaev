1f2e691eefe91f276dd3d0e3077777f6300f9b53
alexander-petr <petryaev.al2005@mail.ru>
2020-12-28 17:58:04 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,14 +2,16 @@ from random import randint as ri
 print('find the mystery number')
 mysterynumber = ri(0, 100)
 guessed = False
-while guessed != True:
+lives = 10
+while guessed != True and lives != 0 :
+    lives -= 1
     answer = int(input('the mystery number is '))
     if answer == mysterynumber:
-        print('\nu are so lucky')
-        guessed = True
+            print('\nu are so lucky')
+            guessed = True
     elif answer > mysterynumber:
-        print('\nthe mystery number is smaller')
+            print('\nthe mystery number is smaller')
     else:
-        print('\nthe mystery number is bigger')
-print('u DID it!!!!!')
+            print('\nthe mystery number is bigger')
+print('The End')
 
